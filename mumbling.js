fdccec3168e97481b4f6bb2a5732fe9e1c315218
yannelly mercado <yannellym@gmail.com>
2022-05-08 15:22:53 -0400

    mumbling problem

---
+++ mumbling.js
@@ -0,0 +1,21 @@
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+
+
+function accum(s) { 
+    let res = [];
+    for(let i=0; i<s.length; i++){
+      let block =  s.split("").map( letter => letter.toUpperCase() + s[i].toLowerCase().repeat(i+0) + "-").slice(i,i+1);
+      res += block;
+    }
+    return res.slice(0, res.length-1)
+  }
+  
+  
+  accum("ZpglnRxqenU") ///"Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
