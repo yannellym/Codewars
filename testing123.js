dd2348bbc3f13a38e37c207f13a2e6426add2300
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-05-21 17:37:26 -0400

    Create testing123.js

---
+++ testing123.js
@@ -0,0 +1,21 @@
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples:
+
+// number([]) // => []
+// number(["a", "b", "c"]) // => ["1: a", "2: b", "3: c"]
+
+
+var number=function(array){
+  let obj = [];
+  
+  for(let i=0; i <array.length; i++){
+    let char = array[i]
+    obj.push([i +1] + ": " + char)
+  }
+  return obj
+}
