68bd8b777b0989994331791dc6cb19ce5a1c6d0a
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-06-21 20:26:23 -0400

    Create maxtreeValueCount.js

---
+++ maxtreeValueCount.js
@@ -0,0 +1,128 @@
+// tree value count
+// Write a function, treeValueCount, that takes in the root of a binary tree and a target value. The function should return the number of times that the target occurs in the tree.
+
+// test_00:
+// const a = new Node(12);
+// const b = new Node(6);
+// const c = new Node(6);
+// const d = new Node(4);
+// const e = new Node(6);
+// const f = new Node(12);
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+
+// //      12
+// //    /   \
+// //   6     6
+// //  / \     \
+// // 4   6     12
+
+// treeValueCount(a,  6); // -> 3
+// test_01:
+// const a = new Node(12);
+// const b = new Node(6);
+// const c = new Node(6);
+// const d = new Node(4);
+// const e = new Node(6);
+// const f = new Node(12);
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+
+// //      12
+// //    /   \
+// //   6     6
+// //  / \     \
+// // 4  6     12
+
+// treeValueCount(a,  12); // -> 2
+// test_02:
+// const a = new Node(7);
+// const b = new Node(5);
+// const c = new Node(1);
+// const d = new Node(1);
+// const e = new Node(8);
+// const f = new Node(7);
+// const g = new Node(1);
+// const h = new Node(1);
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+// e.left = g;
+// f.right = h;
+
+// //      7
+// //    /   \
+// //   5     1
+// //  / \     \
+// // 1   8     7
+// //    /       \
+// //   1         1
+// treeValueCount(a, 1); // -> 4
+// test_03:
+// const a = new Node(7);
+// const b = new Node(5);
+// const c = new Node(1);
+// const d = new Node(1);
+// const e = new Node(8);
+// const f = new Node(7);
+// const g = new Node(1);
+// const h = new Node(1);
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+// e.left = g;
+// f.right = h;
+
+// //      7
+// //    /   \
+// //   5     1
+// //  / \     \
+// // 1   8     7
+// //    /       \
+// //   1         1
+
+// treeValueCount(a, 9); // -> 0
+// test_04:
+// treeValueCount(null, 42); // -> 0
+
+
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+const treeValueCount = (root, target) => {
+  if(root === null) return 0
+  let res = [];
+  let queue = [ root ];
+  
+  while(queue.length >0){
+    let current = queue.pop();
+    if(current.val === target) res.push(current.val)
+    
+    if(current.left !== null) queue.push(current.left)
+     if(current.right !== null) queue.push(current.right)
+  }
+  return res.length
+};
+
+module.exports = {
+  treeValueCount,
+};
