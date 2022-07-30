c71ef25570014054ca34b413ff35746fd5a8c0a1
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-07-30 17:26:35 -0400

    Create correctmistake.py

---
+++ correctmistake.py
@@ -0,0 +1,24 @@
+Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+S is misinterpreted as 5
+O is misinterpreted as 0
+I is misinterpreted as 1
+The test cases contain numbers only by mistake.
+
+
+def correct(s):
+    answer = ""
+    for letter in s.lower():
+        if letter == "5":
+            answer+= "s"
+        elif letter == "0":
+            answer+= "O"
+        elif letter == "1":
+            answer += "I"
+        else:
+            answer += letter
+    return answer.upper()
