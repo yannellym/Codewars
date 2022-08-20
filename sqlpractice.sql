c7abfd54e742deb42b31f5d9be23a9d684ab8720
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-08-19 22:33:50 -0400

    Create sqlpractice.sql

---
+++ sqlpractice.sql
@@ -0,0 +1,9 @@
+SELECT r.name region, a.name account, 
+       o.total_amt_usd/(o.total + 0.01) unit_price
+FROM region r
+JOIN sales_reps s
+ON s.region_id = r.id
+JOIN accounts a
+ON a.sales_rep_id = s.id
+JOIN orders o
+ON o.account_id = a.id;
