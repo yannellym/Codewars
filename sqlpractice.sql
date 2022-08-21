62b0574df3e9491cc637b049b09d2df940b171f5
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-08-20 22:02:54 -0400

    Update sqlpractice.sql

--- sqlpractice.sql
+++ sqlpractice.sql
@@ -7,3 +7,18 @@ JOIN accounts a
 ON a.sales_rep_id = s.id
 JOIN orders o
 ON o.account_id = a.id;
+
+
+#####
+
+SELECT r.name Region, a.name Account, o.total_amt_usd/(o.total + 0.01) unit_price
+FROM orders o
+JOIN accounts a
+ON o.account_id = a.id
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+JOIN region r
+ON s.region_id = r.id
+WHERE o.standard_qty > 100 
+AND poster_qty > 50
+ORDER BY unit_price
