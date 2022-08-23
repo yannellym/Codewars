81b7cec6164f0de8a7011ff185d1593d7c625f3f
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-08-23 13:16:03 -0400

    Update sqlpractice.sql

--- sqlpractice.sql
+++ sqlpractice.sql
@@ -42,3 +42,9 @@ FROM accounts
 SELECT date, (SUBSTR(date, 1,2) || '-' || SUBSTR(date, 4,2) || '-' || SUBSTR(date, 7,4))::date AS new_date
 FROM sf_crime_data
 LIMIT 20
+
+# COALESCE 
+SELECT COALESCE(o.id, a.id) filled_id, a.name, a.website, a.lat, a.long, a.primary_poc, a.sales_rep_id, COALESCE(o.account_id, a.id) account_id, o.occurred_at, COALESCE(o.standard_qty, 0) standard_qty, COALESCE(o.gloss_qty,0) gloss_qty, COALESCE(o.poster_qty,0) poster_qty, COALESCE(o.total,0) total, COALESCE(o.standard_amt_usd,0) standard_amt_usd, COALESCE(o.gloss_amt_usd,0) gloss_amt_usd, COALESCE(o.poster_amt_usd,0) poster_amt_usd, COALESCE(o.total_amt_usd,0) total_amt_usd
+FROM accounts a
+LEFT JOIN orders o
+ON a.id = o.account_id;
