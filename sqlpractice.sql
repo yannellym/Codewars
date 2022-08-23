93d43e05aef83b0623794c1360781fc57756156d
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-08-22 21:05:50 -0400

    Update sqlpractice.sql

--- sqlpractice.sql
+++ sqlpractice.sql
@@ -30,3 +30,9 @@ FROM (SELECT name, CASE WHEN LEFT(UPPER(name), 1) IN ('0','1','2','3','4','5','6
          CASE WHEN LEFT(UPPER(name), 1) IN ('0','1','2','3','4','5','6','7','8','9') 
                        THEN 0 ELSE 1 END AS letter
       FROM accounts) t1;
+
+
+# Each company in the accounts table wants to create an email address for each primary_poc. The email address should be the first name of the primary_poc . last name primary_poc @ company name .com.
+
+SELECT primary_poc, CONCAT(LEFT(primary_poc, STRPOS(primary_poc, ' ') - 1),'.', RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')),'@', name, '.com')
+FROM accounts
