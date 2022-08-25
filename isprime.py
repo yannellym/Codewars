abf3f78b282337d9dc477c2b446cff0952e26a4d
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-08-25 10:47:49 -0400

    Update isprime.py

--- isprime.py
+++ isprime.py
@@ -7,3 +7,21 @@ def is_prime(n):
       return False
     
   return True
+
+###################
+
+
+from math import sqrt, floor
+
+def is_prime(n):
+  if n < 2:
+    return False
+  
+  for i in range(2, floor(sqrt(n)) + 1):
+    if n % i == 0:
+      return False
+    
+  return True
+n = input number
+Time: O(square_root(n))
+Space: O(1)
