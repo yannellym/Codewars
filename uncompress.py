44c5b0eb4331a8192dc0a4a721c665145c7148d4
Yannelly M <91508647+yannellym@users.noreply.github.com>
2022-08-25 11:46:38 -0400

    Create uncompress.py

---
+++ uncompress.py
@@ -0,0 +1,16 @@
+def uncompress(s):
+  nums = '0123456789'
+  result = []
+  i = 0
+  j = 0
+  
+  while  j < len(s):
+    if s[j] in nums:
+      j += 1 
+    else:
+      num = int(s[i:j])
+      result.append(s[j] * num)
+      j += 1
+      i = j
+      
+  return ''.join(result)
